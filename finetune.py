e5929229b4071d5db34fbf8bde7251999944ec71
sooraj331 <148676519+sooraj331@users.noreply.github.com>
2026-02-27 22:15:14 +0530

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -15,7 +15,7 @@ def is_stream_valid(url):
         r = requests.head(url, timeout=6, allow_redirects=True)
 
         # Keep these status codes
-        if r.status_code in (200,451):   # Removed 404,403
+        if r.status_code in (200):   # Removed 404,403,451
             return True
 
         return False
